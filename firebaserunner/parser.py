15183e9d4cddfbd48fb9170ed41d7df2210d1a39
william.wu <william.wu@docusign.com>
2018-11-29 13:13:47 -0800

    remove parser

--- firebaserunner/parser.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse('../test_result/test_result_1.xml')
-root = tree.getroot()
-
-print('Tests Ran: ' + root.attrib['tests'])
-print('Errors: ' + root.attrib['failures'])
-print('Skipped: ' + root.attrib['skipped'])
